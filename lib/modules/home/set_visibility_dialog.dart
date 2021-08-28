cd5b30517721f039bed332ed44bae876364e1a6d
ychsue <hsuy1234@hotmail.com>
2021-08-28 21:43:50 +0800

    可以重排了

--- lib/modules/home/set_visibility_dialog.dart
+++ lib/modules/home/set_visibility_dialog.dart
@@ -4,6 +4,7 @@ import 'package:class_grades_analyzer/controllers/my_global_controller.dart';
 import 'package:class_grades_analyzer/data/model/dimensions/each_case_model.dart';
 import 'package:class_grades_analyzer/data/model/dimensions/tab_names.dart';
 import 'package:class_grades_analyzer/data/model/pair.dart';
+import 'package:class_grades_analyzer/modules/home/mytab_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -38,42 +39,70 @@ class SetVisibilityDialog extends StatelessWidget {
     final c = (currentAxis?.every((ele) => ele.t2) ?? false).obs;
     return Container(child: Obx(
       () {
-        return SimpleDialog(
-          titlePadding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-          title: Container(
-            color: Colors.lime,
-            padding: EdgeInsets.fromLTRB(0, 24, 24, 0),
-            child: ListTile(
-              key: UniqueKey(),
-              leading: Checkbox(
-                  value: c.value,
-                  onChanged: (v) {
-                    debugPrint("Axis: $v");
-                    c.value = v ?? false;
-                    axes.value.toSelAll(axisEnum: whichAxis, isAll: c.value);
+        return Dialog(
+          elevation: 10,
+          child: Flex(direction: Axis.vertical, children: [
+            Container(
+              color: Colors.lime,
+              padding: EdgeInsets.fromLTRB(0, 24, 24, 0),
+              child: ListTile(
+                key: UniqueKey(),
+                leading: Checkbox(
+                    value: c.value,
+                    onChanged: (v) {
+                      debugPrint("Axis: $v");
+                      c.value = v ?? false;
+                      axes.value.toSelAll(axisEnum: whichAxis, isAll: c.value);
+                      axes.refresh();
+                    }),
+                title: Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                          MyGlobalController
+                              .Tabs[TabNames.name(whichAxis)]?.icon,
+                          size: context.theme.textTheme.headline3?.fontSize ??
+                              14),
+                      SizedBox(width: 14),
+                      Text(
+                        TabNames.name(whichAxis),
+                        style: context.theme.textTheme.headline3,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Divider(),
+            Expanded(
+              child: ReorderableListView(
+                  children: [
+                    if (currentAxis != null)
+                      ...currentAxis.map((e) => ListTile(
+                          key: ValueKey(
+                              jsonEncode(e) + TabNames.name(axes.value.main)),
+                          leading: Checkbox(
+                            onChanged: (v) {
+                              e.t2 = !e.t2;
+                              c.value =
+                                  currentAxis?.every((ele) => ele.t2) ?? false;
+                              axes.refresh();
+                              debugPrint("${jsonEncode(e)}");
+                            },
+                            value: e.t2,
+                          ),
+                          title: Text(jsonEncode(e.t1))))
+                    else
+                      ...[]
+                  ],
+                  onReorder: (o, n) {
+                    currentAxis?.insert(n, currentAxis[o]);
+                    currentAxis?.removeAt((o > n) ? (o + 1) : o);
                     axes.refresh();
                   }),
-              title: Text(TabNames.name(whichAxis)),
             ),
-          ),
-          children: [
-            Divider(),
-            if (currentAxis != null)
-              ...currentAxis.map((e) => ListTile(
-                  key: ValueKey(jsonEncode(e) + TabNames.name(axes.value.main)),
-                  leading: Checkbox(
-                    onChanged: (v) {
-                      e.t2 = !e.t2;
-                      c.value = currentAxis?.every((ele) => ele.t2) ?? false;
-                      axes.refresh();
-                      debugPrint("${jsonEncode(e)}");
-                    },
-                    value: e.t2,
-                  ),
-                  title: Text(jsonEncode(e.t1))))
-            else
-              ...[]
-          ],
+          ]),
         );
       },
     ));
